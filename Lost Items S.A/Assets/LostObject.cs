dc56c1a004858f8980875dc3b8d9ae609e482528
Mario Fernández Villalba <mario_bcn2@hotmail.es>
2021-01-29 16:46:48 +0100

    Add LostObject class

---
+++ Lost Items S.A/Assets/LostObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostObject : MonoBehaviour
+{
+
+    enum LostObjectType
+    {
+        FOO_1,
+        FOO_2,
+        FOO_3
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
